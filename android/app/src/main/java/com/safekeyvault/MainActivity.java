361a06b0674466205b7e8681eb861b1147883452
andrea.pacchioni <andrea.pacchioniap@gmail.com>
2023-12-20 15:19:10 +0100

    app icon and splashscreen

--- android/app/src/main/java/com/safekeyvault/MainActivity.java
+++ android/app/src/main/java/com/safekeyvault/MainActivity.java
@@ -1,9 +1,11 @@
 package com.safekeyvault;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +16,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SafeKeyVault";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 
   /**
